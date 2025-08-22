92c636ff3c45d150b03190b8ee4c9f926a733323
Jesus Salcedo <jesalceg@gmail.com>
2025-08-22 12:15:54 -0500

    fix (sequelize): adjust the error in sequelize config ES modules

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,9 +1,11 @@
-export default {
+require("dotenv").config();
+
+module.exports = {
   development: {
-    username: "postgres",
-    password: "postgres",
-    database: "rickmorty",
-    host: "db",
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS || "postgres",
+    database: process.env.DB_NAME || "rickmorty",
+    host: process.env.DB_HOST || "db",
     dialect: "postgres",
   },
 };
